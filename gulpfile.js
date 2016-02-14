45c5b7b0922f6c6ddb31fb921f28d184a1653ae1
Katie Porter <katie@janekporter.com>
2016-02-13 22:53:40 -0700

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+var gulp        = require('gulp'),
+    util        = require('gulp-util'),
+    sass        = require('gulp-sass'),
+    jade        = require('gulp-jade'),
+    browserSync = require('browser-sync').create(),
+    notify      = require('gulp-notify');
+
+gulp.task('default', ['jade', 'sass', 'watch'], function() {
+  return browserSync.init({ server: "./build" });
+});
+
+gulp.task('sass', function() {
+  return gulp.src('./src/sass/**/*.sass')
+    .pipe(sass({includePaths: ['node_modules/modularscale-sass/stylesheets']}))
+    .on('error', notify.onError(function (error) {
+       return 'An error occurred while compiling SASS.\n' + error;
+    }))
+    .pipe(gulp.dest('./build/assets/css'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('jade', function() {
+  return gulp.src('./src/jade/**/*.jade')
+    .pipe(jade())
+    .pipe(gulp.dest('./build'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch('./src/sass/**/*.sass', ['sass']);
+  gulp.watch('./src/jade/**/*.jade', ['jade']);
+  gulp.watch('./build/*.html').on('change', browserSync.reload);
+});
